2dfafe6b171acc808ece97c10c847b38ade5fbf0
Nadeem Sk <nadeem.bs@gmail.com>
2024-09-05 07:53:40 +0530

    init comit

---
+++ code/03. The modern .NET Logger_3.1/logging-course-final-master/2.GettingStarted/BasicConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+
+#region secret 🤭
+
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddConsole();
+});
+
+ILogger<Program> CreateLogger()
+{
+    return loggerFactory.CreateLogger<Program>();
+}
+#endregion
+
+ILogger logger = CreateLogger();
+
+logger.LogInformation("Hello world!");
